ea196f3d1a7923261f758ffe864146a9a7cae6cf
Leonardo <leozikasantos73@gmail.com>
2021-04-02 12:32:51 -0300

    config express server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('test');
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+app.use('/', express.static(resolve(__dirname,'./build' )))
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err) { return console.log(err)  }
+
+    console.log('The express is working :D')
+})
